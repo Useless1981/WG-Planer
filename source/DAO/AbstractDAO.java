f87436b30ea4396b8a2538cd47493649c5b4518f
Jan Giercke <jan@giercke.de>
2021-01-11 10:45:25 +0100

    Created Base Structure of java calsses for the DAO package

---
+++ source/DAO/AbstractDAO.java
@@ -0,0 +1,21 @@
+package DAO;
+
+public abstract class AbstractDAO<T> {
+
+
+    public <T> T save(T entity) {
+
+    }
+
+    public <T> void update(T entity) {
+
+    }
+
+    public <T> T getByID(long ID) {
+
+    }
+
+    public <T> void delete(T entity) {
+
+    }
+}
