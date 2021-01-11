42fc1dc3ac5fba860289645053601aeb474b6142
Jan Giercke <jan@giercke.de>
2021-01-11 10:58:41 +0100

    updated View and Controller added Benutzer.java class

--- source/Controller/AbstractController.java
+++ source/Controller/AbstractController.java
@@ -2,5 +2,10 @@ package Controller;
 
 import java.awt.event.ActionListener;
 
+import Model.Benutzer;
+import View.AbstractView;
+
 public abstract class AbstractController implements ActionListener {
+    private Benutzer benutzer;
+    private AbstractView view;
 }
