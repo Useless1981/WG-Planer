6fd6bd7cf12ddde7bc12c08304af9dfb91c8e1dc
Jan Giercke <jan@giercke.de>
2021-01-11 12:55:58 +0100

    implemented Termin

--- source/Model/Termin.java
+++ source/Model/Termin.java
@@ -1,5 +1,31 @@
 package Model;
 
+import java.util.Date;
+
 public class Termin {
-    // to be implemented
+    private int ID;
+    private boolean wiederkehrend;
+    private Date date;
+
+    public Termin(int ID, boolean wiederkehrend, Date date) {
+        this.date = date;
+        this.ID = ID;
+        this.wiederkehrend = wiederkehrend;
+    }
+
+    public boolean isWiederkehrend() {
+        return wiederkehrend;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setWiederkehrend(boolean wiederkehrend) {
+        this.wiederkehrend = wiederkehrend;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
